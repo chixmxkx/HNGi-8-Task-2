0171496c873c93624d8e666ce87edca15b7fac1a
chixmxkx <chiamakaessonna@gmail.com>
2021-08-19 22:33:57 +0100

    new changes

--- 2/index.php
+++ 2/index.php
@@ -317,7 +317,7 @@
             <div class="h6"><i class="far fa-envelope pe-2 text-muted" style="width:24px;opacity:0.85;"></i> beautifulessonna@gmail.com</div>
           </div>
           <div class="mt-5 d-print-none">
-  <form method="POST" action="contactme.php">
+  <form method="POST" action="index.php">
   <div class="form-outline mb-4">
     <input type="text" id="name" name="name" class="form-control" required/>
     <label class="form-label" for="name">Name</label>
